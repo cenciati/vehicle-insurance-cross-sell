8043ec0541c7dddb4a7a3b0af5078872f434e727
Gabriel <gbrcenciati@gmail.com>
2022-09-06 21:12:53 -0300

    add: test status code; predict scores; data format

--- src/api/tests/test_predict.py
+++ src/api/tests/test_predict.py
@@ -1,14 +1,17 @@
+import json
+
+from fastapi import status
 from fastapi.testclient import TestClient
+
 from src.api.mocks.mock_predict import mock_predict
 from src.api.routes.predict import predict
-import json
 
 # initialize testclient object class
 client = TestClient(predict)
 
 # create mocked test data
-size = 10
-data_mocked = mock_predict(size=size)
+SIZE = 10
+data_mocked = mock_predict(size=SIZE)
 
 # create manual test data
 data_manual = [
@@ -41,32 +44,53 @@ data_manual = [
 ]
 
 
-def test_predict_with_single_item() -> None:
-    """makes a request with a single item"""
+# testing status code
+def test_if_status_code_is_equal_200_with_a_single_item() -> None:
+    """checks if the response status code is equal to 200 with a single item"""
     response = client.post(url="/", data=json.dumps(data_manual[0]))
+    assert response.status_code == status.HTTP_200_OK
 
-    assert response.status_code == 200
+    return None
+
+
+def test_if_status_code_is_equal_200_with_multiple_items() -> None:
+    """checks if the response status code is equal to 200 with multiple tems"""
+    response = client.post(url="/", data=json.dumps(data_manual))
+    assert response.status_code == status.HTTP_200_OK
+
+    return None
+
+
+def test_if_status_code_is_equal_200_with_multiple_mocked_items() -> None:
+    """checks if the response status code is equal to 200 with multiple random items"""
+    response = client.post(url="/", data=json.dumps(data_mocked))
+    assert response.status_code == status.HTTP_200_OK
+
+    return None
+
+
+# testing predicted scores
+def test_predict_with_a_single_item() -> None:
+    """checks the predicted score to a single item"""
+    response = client.post(url="/", data=json.dumps(data_manual[0]))
     assert response.json()["381110"] == 0.058194444444444444
 
     return None
 
 
-def test_predict_with_list_of_items() -> None:
-    """makes a request with multiple items"""
+def test_predict_with_multiple_items() -> None:
+    """checks the predicted score to multiple items"""
     response = client.post(url="/", data=json.dumps(data_manual))
-
-    assert response.status_code == 200
     assert response.json()["293843"] == 0.26901463886990207
     assert response.json()["381110"] == 0.058194444444444444
 
     return None
 
 
-def test_predict_random_mock_data() -> None:
-    """makes a request with random items"""
+# test response data format
+def test_if_json_format_is_equal_to_json() -> None:
+    """checks if the response type is a json object"""
     response = client.post(url="/", data=json.dumps(data_mocked))
-
-    assert response.status_code == 200
-    # assert len(response.json()) == size
+    assert response.headers["Content-Type"] == "application/json"
 
     return None
